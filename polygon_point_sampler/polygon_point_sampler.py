52558f08bb041c1dd2beac5981375fe652645411
leaffan <leaffan@gmx.net>
2018-02-21 14:10:30 +0100

    Apply Python 3 coding syntax and principles

--- polygon_point_sampler/polygon_point_sampler.py
+++ polygon_point_sampler/polygon_point_sampler.py
@@ -1,22 +1,18 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# File: polygon_point_sampler.py
-# Author: Markus Reinhold
-# Contact: leaffan@gmx.net
-# Creation Date: 2011/10/04 10:40:44
-
-u"""
+"""
 A base class for creating sample points located in a given region of interest,
 i.e. polygon.
 """
 
 from shapely.geometry import Polygon
 
+
 class PolygonPointSampler(object):
-    
-    def __init__(self, polygon = ''):
-        u"""
+
+    def __init__(self, polygon=''):
+        """
         Initialize a new PolygonPointSampler object using the specified polygon
         object (as allocated by Shapely). If no polygon is given a new empty
         one is created and set as the base polygon.
@@ -30,25 +26,25 @@ class PolygonPointSampler(object):
         self.prepared = False
 
     def add_polygon(self, polygon):
-        u"""
-        Add another polygon entity to the base polygon by geometrically unifying
-        it with the current one.
+        """
+        Add another polygon entity to the base polygon by geometrically
+        unifying it with the current one.
         """
         self.polygon = self.polygon.union(polygon)
         self.prepared = False
 
     def print_samples(self):
-        u"""
+        """
         Print all sample points using their WKT representation.
         """
         for sample_pt in self.samples:
-            print sample_pt
+            print(sample_pt)
 
     def prepare_sampling(self):
-        u"""
-        Prepare the actual sampling procedure by splitting up the specified base
-        polygon (that may consist of multiple simple polygons) and appending its
-        compartments to a dedicated list.
+        """
+        Prepare the actual sampling procedure by splitting up the specified
+        base polygon (that may consist of multiple simple polygons) and
+        appending its compartments to a dedicated list.
         """
         self.src = list()
         if hasattr(self.polygon, 'geoms'):
@@ -59,7 +55,7 @@ class PolygonPointSampler(object):
         self.prepared = True
 
     def perform_sampling(self):
-        u"""
+        """
         Create a stub for the actual sampling procedure.
         """
         raise NotImplementedError
