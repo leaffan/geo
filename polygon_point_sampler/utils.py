a21fb46ced771f3267e70e917cd1a35587493367
leaffan <leaffan@gmx.net>
2018-02-21 14:01:22 +0100

    Remove unnecessary docstrings

--- polygon_point_sampler/utils.py
+++ polygon_point_sampler/utils.py
@@ -1,17 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# File: ....py
-# Author: Markus Reinhold
-# Contact: leaffan@gmx.net
-# Creation Date: 2012/03/13 13:15:37
-
-u"""
-... Put description here ...
-"""
-
 from random import random
 from bisect import bisect_right
+
 
 class WeightedRandomGenerator():
     def __init__(self, weights):
@@ -20,13 +12,14 @@ class WeightedRandomGenerator():
         for w in weights:
             running_total += w
             self.totals.append(running_total)
-    
+
     def next(self):
         rnd = random() * self.totals[-1]
         return bisect_right(self.totals, rnd)
-        
+
     def __call__(self):
         return self.next()
+
 
 def floatrange(start, stop, step):
     while start < stop:
